d82676bd6852239450d628edf9cdf2602d5adc8c
mqislearning <145309965+mqislearning@users.noreply.github.com>
2025-07-20 22:59:47 +0800

    submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -157,8 +157,8 @@ def send_signed_msg(proof, random_leaf):
     tx = contract.functions.submit(proof, random_leaf).build_transaction({
         'from': acct.address,
         'nonce': nonce,
-        'gas': 2000000,
-        'gasPrice': w3.to_wei('50', 'gwei')
+        'gas': 300000,
+        'gasPrice': w3.to_wei('5', 'gwei')
     })
 
     signed_tx = w3.eth.account.sign_transaction(tx, acct.key)
